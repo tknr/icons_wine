f644178a333e38deb4ab0a7c66768fa3f13a633f
tknr <geishagirlskoume@gmail.com>
2019-08-08 23:53:47 +0900

    2019年  8月  8日 木曜日 23:53:47 JST

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /usr/local/share/wine/icons || exit 1
 
-for dir in `ls -l | grep ^d | cut -d " " -f 10`
+for dir in `find . -mindepth 1 -type d | grep -v git | cut -d "/" -f 2`
 do
 	cp -Rf ${dir} /usr/local/share/wine/icons/
 done
